87db58d47e3e42d507b9ad70bf73498c82ad2c17
Hrparmar <47150173+Hrparmar@users.noreply.github.com>
2019-02-15 10:14:26 -0500

    Update math.java

--- math.java
+++ math.java
@@ -1,4 +1,4 @@
-public class hello {
+public class math {
     public static void main (String [] args){
     System.out.println("My first project");
         System.out.println("this is done by hetal");
