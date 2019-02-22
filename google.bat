ca1e68bc2ebacc885d9ae8ad8d494d907a3437b1
Hrparmar <47150173+Hrparmar@users.noreply.github.com>
2019-02-21 20:39:13 -0500

    Update google.bat

--- google.bat
+++ google.bat
@@ -1,4 +1,11 @@
-set A/ num1=%num1%
-set A/ num2=%num2%
-set A/ sum=%num1%+%num2%
+
+set /A num1=%num1%
+set /A num2=%num2%
+set /A sum=num1+num2
 echo %sum%
+set /A sub=num1-num2
+echo %sub%
+set /A multi=num1*num2
+echo %multi%
+set /A divide=num1/num2
+echo %divide% 
